cc913a33cf1018859e405c40e3dc227972fd8b4c
Your Name <CHANNY.CHHUN@PNC.LAN>
2024-04-24 19:58:46 +0700

    write code class passenger baggage and flightNumber

---
+++ dist/src/flightNumber/flightNumber.ts
@@ -0,0 +1,9 @@
+export class flightNumber {
+    private AlineCode: String
+    private FlightCode: String
+    constructor(AlineCode: String, FlightCode: String) {
+        this.AlineCode = AlineCode
+        this.FlightCode = FlightCode
+
+    }
+}
