877f338a4ac8a2ce3b5892e421e4d89112520313
Radit They <rdit.thy@student.passerellesnumeriques.org>
2024-04-25 08:33:29 +0700

    create branch flightClass and write code on Flight

---
+++ dist/src/Flight/flight.ts
@@ -0,0 +1,20 @@
+import { flightNumber } from "../flightNumber/flightNumber";
+
+class Flight {
+    private FlightId: string;
+    private FlightNumber: flightNumber;
+    private StartTime: Date;
+    private ReachingTime: Date; //start time until arrive time
+    private Destination: string;
+    private Price: number;
+    constructor(FlightId:string, FlightNumber:flightNumber, StartTime:Date,Destination:string, ReachingTime:Date,Price:number) {
+        this.FlightId = FlightId;
+        this.FlightNumber = FlightNumber;
+        this.StartTime = StartTime;
+        this.ReachingTime = ReachingTime;
+        this.Destination = Destination;
+        this.Price = Price;
+        
+    }
+    public FlightDetails(){};
+}
